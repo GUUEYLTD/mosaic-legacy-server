32434da0c47a0c209ad9413ad6eac2a3b5d31ed7
TovarishFin <tovarishfin@yandex.ru>
2016-09-02 11:00:26 +0200

    test route for polymer ajax

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 /*
+
+*/
 var firebase= require("firebase");
 //import firebase module
 var firebase = require("firebase");
@@ -14,10 +16,16 @@ var ref = db.ref("/userIndex/vrD2wmdFs7dArOcXCIMJeJvSLAy2/home");
 ref.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
-*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "firebase test"});
 });
 
+//test with polymer iron-ajax
+router.get('/test', function(req,res,next){
+  for(x in req.query){
+    req.query[x]+=" modded";
+  };
+  res.json(req.query);
+});
 module.exports = router;
