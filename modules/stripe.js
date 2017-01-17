a69f93c94c06130d631934632d1103fefdb2f58b
TovarishFin <tovarishfin@gmail.com>
2017-01-17 14:02:44 +0100

    updated to live stripe payment api

--- modules/stripe.js
+++ modules/stripe.js
@@ -1,4 +1,4 @@
-var stripe = require("stripe")("sk_test_wy2eqgGLc0xhCu32bMU1xVrc");
+var stripe = require("stripe")("sk_live_6naCZ8A78iR8dDLIyYmDeev8");
 var db = require("./firebase");
 
 module.exports={
