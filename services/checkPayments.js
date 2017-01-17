d4e4a21180ffa8afef9045cc323eb5085afb5d5e
TovarishFin <tovarishfin@gmail.com>
2017-01-17 15:14:36 +0100

    updated forgotten test key on checkPayments.js

--- services/checkPayments.js
+++ services/checkPayments.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var db = require("../modules/firebase");
-var stripe = require("stripe")("sk_test_wy2eqgGLc0xhCu32bMU1xVrc");
+var stripe = require("stripe")("sk_live_6naCZ8A78iR8dDLIyYmDeev8");
 
 var self = module.exports={
 
