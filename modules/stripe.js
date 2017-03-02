63f92486fee384539d019efbc3131a8eaff095d7
TovarishFin <tovarishfin@gmail.com>
2017-03-02 13:55:37 +0100

    fixed incorrect db module paths

--- modules/stripe.js
+++ modules/stripe.js
@@ -1,5 +1,6 @@
-var stripe = require("stripe")("sk_live_6naCZ8A78iR8dDLIyYmDeev8");
-var db = require("./firebase");
+var stripe = require("stripe")("sk_test_wy2eqgGLc0xhCu32bMU1xVrc");
+
+var db = require("./firebase").db;
 
 module.exports={
 
